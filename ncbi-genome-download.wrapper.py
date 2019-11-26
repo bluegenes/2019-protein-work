482308cfdce50d1cb786b8617cda6d619f8296f3
N Tessa Pierce <ntpierce@gmail.com>
2019-11-26 15:53:50 -0800

    use try-except to escape file existence issues

--- ncbi-genome-download.wrapper.py
+++ ncbi-genome-download.wrapper.py
@@ -20,57 +20,49 @@ if accession:
 
 # handle outputs
 genomic_out = snakemake.output.get("genomic")
-prot_out = snakemake.output.get("protein")
+protein_out = snakemake.output.get("protein")
 rna_out = snakemake.output.get("rna")
 cds_out = snakemake.output.get("cds")
-outfiles = [genomic_out, prot_out, rna_out, cds_out]
+outfiles = [genomic_out, protein_out, rna_out, cds_out]
 assert (list(map(bool, outfiles)).count(True) >= 1), "please specify at least one output format by using the 'genomic', 'protein', 'rna', 'cds' keywords in the output field)"
-
-format_list = []
-if genomic_out:
-    format_list.append("fasta")
-if prot_out:
-    format_list.append("protein-fasta")
-if rna_out:
-    format_list.append("rna-fna")
-if cds_out:
-    format_list.append("cds-fasta")
-
-formats = ",".join(format_list)
-format_cmd= f" --format {formats} "
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 dl_dir=os.path.join("genbank","*",accession)
 
-# run ncbi-genome-download
-shell("ncbi-genome-download all -A {accession} {format_cmd} -s genbank -p {snakemake.threads} {log}")
-
-# move output to the right spot
+# run ncbi-genome-download and if it works,  move output to the right spot
 if genomic_out:
     tmp_genomic=os.path.join(dl_dir,"*_genomic.fna.gz")
-    shell("mv {tmp_genomic} {genomic_out}")
-else:
-    shell("touch {genomic_out}")
-    shell("echo {genomic_out} >> ncbi_failed_downloads.txt")
+    try:
+        shell("ncbi-genome-download all -A {accession} --format 'fasta' -s genbank -p {snakemake.threads} {log}")
+        shell("mv {tmp_genomic} {genomic_out}")
+    except:
+        shell("touch {genomic_out}")
+        shell("echo {genomic_out} >> ncbi_failed_downloads.txt")
 
 if protein_out:
     tmp_protein=os.path.join(dl_dir,"*_protein.faa.gz")
-    shell("mv {tmp_protein} {protein_out}")
-else:
-    shell("touch {protein_out}")
-    shell("echo {protein_out} >> ncbi_failed_downloads.txt")
+    try:
+        shell("ncbi-genome-download all -A {accession} --format 'protein-fasta' -s genbank -p {snakemake.threads} {log}")
+        shell("mv {tmp_protein} {protein_out}")
+    except:
+        shell("touch {protein_out}")
+        shell("echo {protein_out} >> ncbi_failed_downloads.txt")
 
 if rna_out:
     tmp_rna=os.path.join(dl_dir,"*_rna_from_genomic.fna.gz")
-    shell("mv {tmp_rna} {rna_out}")
-else:
-    shell("touch {rna_out}")
-    shell("echo {rna_out} >> ncbi_failed_downloads.txt")
+    try:
+        shell("ncbi-genome-download all -A {accession} --format 'rna-fna' -s genbank -p {snakemake.threads} {log}")
+        shell("mv {tmp_rna} {rna_out}")
+    except:
+        shell("touch {rna_out}")
+        shell("echo {rna_out} >> ncbi_failed_downloads.txt")
 
 if cds:
     tmp_cds= os.path.join(dl_dir,"*_cds_from_genomic.fna.gz")
-    shell("mv {tmp_cds} {cds_out}")
-else:
-    shell("touch {cds_out}")
-    shell("echo {cds_out} >> ncbi_failed_downloads.txt")
+    try:
+        shell("ncbi-genome-download all -A {accession} --format 'cds-fasta' -s genbank -p {snakemake.threads} {log}")
+        shell("mv {tmp_cds} {cds_out}")
+    except:
+        shell("touch {cds_out}")
+        shell("echo {cds_out} >> ncbi_failed_downloads.txt")
